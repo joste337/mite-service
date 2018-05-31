458e49206053f9d7712b1e6e16b3da056182548f
Jörg Stenger <joerg.stenger@exozet.com>
2018-05-31 13:50:56 +0200

    initial commit

---
+++ src/main/java/de/jos/service/mite/miteservice/MiteServiceApplication.java
@@ -0,0 +1,12 @@
+package de.jos.service.mite.miteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiteServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/de/jos/service/mite/miteservice/MiteServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package de.jos.service.mite.miteservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MiteServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
